1c5ace5a5451f094b8848162ca3a5ae24fcdadd6
Goblins <staff@vt.edu>
2020-03-05 16:22:15 -0500

    lots of changes and function additions to libgoblin :)

--- libgoblin.py
+++ libgoblin.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-import json, subprocess, collections
+import json, subprocess, collections, random, datetime
 cleos = ["cleos", "--no-auto-keosd", "-u", "http://127.0.0.1:8888", "--wallet-url", "unix:///home/ubuntu/eosio-wallet/keosd.sock"]
 KEY = "EOS7J1tYpCHkCCvi5DwYXkJMRKRzK9XAUVvMC7PnrcucNXS6ZuMC1"
 KEYWORDS = {"from", "except", "if", "else", "elif", "return"}
@@ -9,6 +9,10 @@ KEYWORDS = {"from", "except", "if", "else", "elif", "return"}
 ## Restart mako-server every time you change this file ##
 #########################################################
 
+class EosioError(Exception):
+    # TODO - more post-processing, parse out exact error message, etc...
+    def __init__(self, output):
+	self.output = output
 
 def _try_symbolize_names(l):
 	if isinstance(l, list): return [_try_symbolize_names(i) for i in l]
@@ -21,10 +25,15 @@ def _try_symbolize_names(l):
 
 def _exec(l, json_decode = True):
 	if not isinstance(l, list): raise TypeError("Invalid object passed to libgoblin._exec(): {}".format(str))
-	result = subprocess.check_output(cleos + l).decode("utf-8")
+	proc = subprocess.Popen(cleos + l, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = proc.communicate()
+	stdout = stdout.decode("utf-8")
+	stderr = stderr.decode("utf-8")
+	if proc.returncode != 0:
+		raise EosioError(stderr)
 	if json_decode:
-		return _try_symbolize_names(json.loads(result))
-	return result
+		return _try_symbolize_names(json.loads(stdout))
+	return stdout
 
 def get_info():
 	return _exec(["get", "info"])
@@ -80,8 +89,94 @@ def debug_format(t, depth=0, dash=False):
 		return "\n".join([debug_format(x, depth+1, True) for x in t])
 	return firstpad + str(t)
 
-def buy(user, ticket_id):
-	return _exec(["push", "action", "hokipoki", "buy", json.dumps([user, ticket_id]), "-p", user + "@active", "-j"])
+
+#TODO: make it return formatted output
+# USER ACTIONS
+def buy(user, game_id):
+	parsed = get_raw_table("tickets")
+	ticket_id = -1
+	filtered = filter(lambda a:a["game_id"]==game_id and a["for_lottery"]==0 and a["owner"]=="hokipoki",parsed)
+	if len(filtered) > 0:
+		ticket_id= filtered[0]["id"]
+	#TODO: Error if ticket is not available
+	try:
+		_exec(["push", "action", "hokipoki", "buy", json.dumps([user, ticket_id]), "-p", user + "@active", "-j"])
+		return get_balance(user)
+	except EosioError as e:
+		return e.output
+
+
 def sell(user, ticket_id):
 	return _exec(["push", "action", "hokipoki", "sell", json.dumps([user, ticket_id]), "-p", user + "@active", "-j"])
 
+def enter_lottery(user, game_id):
+	r1 = random.randint(0, 2 ** 64 - 1)
+	r2 = random.randint(0, 2 ** 64 - 1)
+	r3 = random.randint(0, 2 ** 64 - 1)
+	r4 = random.randint(0, 2 ** 64 - 1)
+	return _exec(["push", "action", "hokipoki", "enterlottery", json.dumps([user, game_id, r1, r2, r3, r4]), "-p", user + "@active", "-j"])
+
+def leave_lottery(user, game_id):
+	return _exec(["push", "action", "hokipoki", "leavelottery", json.dumps([user, game_id]), "-p", user + "@active", "-j"])
+
+
+# ADMIN ACTIONS
+def execute_lottery(game_id):
+	return _exec(["push", "action", "hokipoki", "executelotto", json.dumps([game_id]), "-p", "hokipoki@active", "-j"])
+
+def open_lottery(game_id):
+	return _exec(["push", "action", "hokipoki", "openlottery", json.dumps([game_id]), "-p", "hokipoki@active", "-j"])
+
+def create_game(day, num_tickets, tickets_for_lotto, price, name, location, lottery_opens, lottery_closes):
+	return _exec(["push", "action", "hokipoki", "creategame", json.dumps([day, num_tickets, tickets_for_lotto, price, name, location, lottery_opens, lottery_closes]), "-p", "hokipoki@active", "-j"])
+
+def reset():
+	return _exec(["push", "action", "hokipoki", "reset", json.dumps([]), "-p", "hokipoki@active"])
+
+
+# USER AND ADMIN ACTIONS
+def tickets():
+	return _exec(["get", "table", "hokipoki", "hokipoki", "tickets", "-l", "-1"])
+
+def games():
+	return _exec(["get", "table", "hokipoki", "hokipoki", "games", "-l", "-1"])
+
+def lottery_entries():
+	return _exec(["get", "table", "hokipoki", "lottoentries", "-l", "-1"])
+
+#REQUESTS RECIEVED FROM RACHEL
+
+#returns all tickets that a user owns
+def user_tickets(user):
+	return filter(lambda a:a["owner"]==user,get_raw_table("tickets"))
+
+#gives you everything, but you can easily index the list for what you want uwu
+def filtered_date_games(date):
+	filtered = filter(lambda a:a["date"]>=date,get_raw_table("games"))
+	return sorted(filtered,key=lambda a:a["date"])
+
+# Returns None if no game exists
+# Returns game json object if game with id==game_id exists
+def get_game(game_id):
+	try:
+		return filter(lambda a:a['id']==game_id,get_raw_table("games"))[0]
+	except:
+		return None
+
+# Returns true if there is a ticket available to be bought for game with id == game_id or false if not
+def is_ticket_available(game_id):
+	today = datetime.datetime.now().strftime("%Y%m%d%H%M")
+	game_time = filter(lambda a:a["id"]==game_id,get_raw_table("games"))[0]["date"]
+	return len(filter(lambda a:a["game_id"]==game_id and a["for_lottery"]==0 and a["owner"]=="hokipoki" and game_time >= today ,get_raw_table("tickets"))) >0
+
+# Returns true if the lottery is open for game with id == game_id or false if not
+def is_lottery_available(game_id):
+	return len(filter(lambda a:a["id"]==game_id and a["lottery_open"]==1,get_raw_table("games"))) > 0
+
+# Returns true if user owns a ticket for game with id == game_id or false if not
+def user_has_ticket(user,game_id):
+    return len(filter(lambda a:a["game_id"] == game_id and a["owner"] == user,get_raw_table("tickets")))>0
+
+# Returns true if user is in lottery for game with id == game_id or false if not
+def user_in_lottery(user,game_id):
+    return len(filter(lambda a:a["user"]==user and a["game_id"]==game_id,get_raw_table("lottoentries")))>0
---
+++ test.py
@@ -0,0 +1,76 @@
+#!python
+from __future__ import print_function
+import json
+from libgoblin import *
+import datetime
+
+# try:
+#     parsed = libgoblin.buy('ankita',3)
+#     print(type(parsed))
+#     print(json.dumps(parsed, indent=4, sort_keys=True))
+# except Exception as e:
+#     #print("TEST HAHAHAHAH GOTEEM")
+#     print(e)
+
+
+# try:
+#     parsed = libgoblin.sell('ankita',3)
+#     print(type(parsed))
+#     print(json.dumps(parsed, indent=4, sort_keys=True))
+# except Exception as e:
+#     print(e)
+
+
+
+
+# game_id = 1
+# parsed = libgoblin.get_raw_table("tickets")
+# filtered = filter(lambda a:a["game_id"]==game_id and a["for_lottery"]==0 and a["owner"]=="hokipoki",parsed)
+# for item in filtered:
+#     print(item)
+
+# game_id = 3
+# user = "bruh"
+
+# a = libgoblin.buy(user,game_id)
+# for item in a[0][1][0][3][0]:
+#     print(item)
+
+# table = libgoblin.get_raw_table("tickets")
+# filtered = filter(lambda a:a["game_id"] == game_id and a["owner"] == user,table)
+# ticket_id = filtered[0]["id"]
+
+# a = libgoblin.sell(user,ticket_id)
+
+# user='bruh'
+# return filter(lambda a:a["owner"]==user,get_raw_table("tickets"))
+
+# date = 20200305
+# filtered = filter(lambda a:a["date"]>=date,get_raw_table("games"))
+# b = sorted(filtered,key=lambda a:a["date"])
+# for item in b:
+#     print(item)
+
+# game_id = 0
+# filtered = filter(lambda a:a['id']==game_id,get_raw_table("games"))[0]
+# print(filtered)
+
+# game_id = 0
+# today = datetime.datetime.now().strftime("%Y%m%d%H%M")
+# game_time = filter(lambda a:a["id"]==game_id,get_raw_table("games"))[0]["date"]
+# filtered = len(filter(lambda a:a["game_id"]==game_id and a["for_lottery"]==0 and a["owner"]=="hokipoki" and game_time >= today ,get_raw_table("tickets"))) >0
+# print(filtered)
+
+# def user_has_ticket(user,game_id):
+#     return len(filter(lambda a:a["game_id"] == game_id and a["owner"] == user,get_raw_table("tickets")))>0
+    
+# print(user_has_ticket("bruh",0))
+# buy("bruh",0)
+# print(user_has_ticket("bruh",0))
+
+def user_in_lottery(user,game_id):
+    return len(filter(lambda a:a["user"]==user and a["game_id"]==game_id,get_raw_table("lottoentries")))>0
+print(user_in_lottery("bruh",0))
+print(user_in_lottery("bruh",1))
+
+#print(json.dumps(parsed,indent=4,sort_keys=True))
