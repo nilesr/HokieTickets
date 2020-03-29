0951e9d368de97a7a7c54dcb4f36de82477456a6
Goblins <staff@vt.edu>
2020-03-29 18:46:32 -0400

    halfway on auctions

--- static/student.js
+++ static/student.js
@@ -1,7 +1,7 @@
 // Format ticket values into HTK (i.e. 6000 -> "60.00 HTK")
 function formatHTK(value) {
     var val = value.toString();
-    document.write(val.slice(0, -2) + "." + val.slice(-2) + " HTK");
+    return val.slice(0, -2) + "." + val.slice(-2) + " HTK";
 }
 
 // Format date from YYYYMMDDHHMM to "Month" DD, YYYY
@@ -42,6 +42,33 @@ function openWindow(user, id, action, info) {
 
     // Modal content
     var body = modal.querySelector(".modal-body");
+
+    // Buttons
+    var buttonArea = modal.querySelector(".button-area");
+    // Find or create Confirm button
+    var confirm = buttonArea.querySelector(".confirmButton");
+    if (confirm == null) {
+        confirm = buttonArea.appendChild(document.createElement("button"));
+    }
+    confirm.innerHTML = "Confirm";
+    confirm.setAttribute("class", "confirmButton");
+    confirm.style.display = "block";
+
+    // Find or create "Go Back" button
+    var back = buttonArea.querySelector(".backButton");
+    if (back == null) {
+        back = buttonArea.appendChild(document.createElement("button"));
+
+        back.innerHTML = "Go Back";
+        back.setAttribute("class", "backButton");
+        back.onclick = function(event) {
+            // Close the modal
+            modal.style.display = "none";
+            location.reload();
+        }
+    }
+
+
     // Find or create title
     var title = body.querySelector("#modalTitle");
     if (title == null) {
@@ -78,29 +105,22 @@ function openWindow(user, id, action, info) {
         text.children[0].innerHTML = "You are selling your ticket for <b>" + info['event_name'] + "</b>.";
         text.children[1].innerHTML = "Your current balance is <b>" + info['current_balance'] + " HTK</b>.";
         text.children[2].innerHTML = "Click <b>Confirm</b> to continue.";
-    }
-
-    var buttonArea = modal.querySelector(".button-area");
-    // Find or create Confirm button
-    var confirm = buttonArea.querySelector(".confirmButton");
-    if (confirm == null) {
-        confirm = buttonArea.appendChild(document.createElement("button"));
-        confirm.innerHTML = "Confirm";
-        confirm.setAttribute("class", "confirmButton");
-    }
-    confirm.style.display = "block";
-
-    // Find or create "Go Back" button
-    var back = buttonArea.querySelector(".backButton");
-    if (back == null) {
-        back = buttonArea.appendChild(document.createElement("button"));
-
-        back.innerHTML = "Go Back";
-        back.setAttribute("class", "backButton");
-        back.onclick = function(event) {
-            // Close the modal
-            modal.style.display = "none";
-            location.reload();
+    } else if (action == "create_auction") {
+        title.innerHTML = "Confirm Ticket Auction";
+        text.children[0].innerHTML = "You are auctioning your ticket for <b>" + info['event_name'] + "</b>.";
+        text.children[1].innerHTML = "The ticket will start at a price of <b>" + formatHTK(info['ticket_value']) + "</b>.";
+        text.children[2].innerHTML = "Click <b>Confirm</b> to continue.";
+    } else if (action == "view_auction") {
+        title.innerHTML = "Auction Details";
+        text.children[0].innerHTML = "Highest bid: <b>" + "" + "</b>.";
+        text.children[1].innerHTML = "Auction end date: <b>" + "</b>.";
+        if (1 < 0) { // Nobody has bid
+            text.children[2].innerHTML = "Nobody has bid in this auction. Click <b>Cancel Auction</b> to reclaim your ticket.";
+            confirm.innerHTML = "Cancel Auction";
+            confirm.setAttribute("style", "background-color: #BB0000;"); // Make button red for dangerous action
+        } else {
+            confirm.style.display = "none";
+            text.children[2].innerHTML = "";
         }
     }
 
@@ -108,6 +128,9 @@ function openWindow(user, id, action, info) {
     // On clicking confirm button, make request to execute action
     confirm.onclick = function(event) {
         // Send necessary information to backend
+        if (action == "view_auction") { // Can only click "confirm" button if they are cancelling auction
+            action = "cancel_auction";
+        }
         var data = {
             action: action,
             user: user,
@@ -115,8 +138,12 @@ function openWindow(user, id, action, info) {
         // Set up variable information
         if (action == "buy" || action == "enter_lottery" || action == "leave_lottery") {
             data['game_id'] = id;
-        } else if (action == "sell") {
+        } else if (action == "sell" || action == "cancel_auction") {
             data['ticket_id'] = id;
+        } else if (action == "create_auction") {
+            data['ticket_id'] = id;
+            data['initial_bid'] = info['ticket_value'];
+            // data['end_date'] = 
         }
 
         // Send the request JSON
@@ -139,14 +166,31 @@ function openWindow(user, id, action, info) {
                 text.children[0].innerHTML = resp.error;
                 text.children[1].innerHTML = "";
                 text.children[2].innerHTML = "";
-            } else if (resp.hasOwnProperty("balance")) { // Successful execution, returned balance
-                text.children[0].innerHTML = "Success! Your new balance is <b>" + resp['balance'] + " HTK</b>.";
-                text.children[1].innerHTML = "";
-                text.children[2].innerHTML = "";
             } else { // Other kind of success
                 text.children[0].innerHTML = "Success!";
-                text.children[1].innerHTML = "";
-                text.children[2].innerHTML = "";
+                if (action == "buy") {
+                    text.children[0].innerHTML += " You have bought a ticket for <b>" + info['event_name'] + "</b>.";
+                    text.children[1].innerHTML = "Your new balance is <b>" + resp['balance'] + " HTK</b>."
+                    text.children[2].innerHTML = "Click <b>Go Back</b> to close this window.";
+                } else if (action == "sell") {
+                    text.children[0].innerHTML += " You have sold your ticket for <b>" + info['event_name'] + "</b>.";
+                    text.children[1].innerHTML = "Your new balance is <b>" + resp['balance'] + " HTK</b>."
+                    text.children[2].innerHTML = "Click <b>Go Back</b> to close this window.";
+                } else if (action == "enter_lottery") {
+                    text.children[0].innerHTML += " You have entered the lottery for <b>" + info['event_name'] + "</b>.";
+                    text.children[1].innerHTML = "Check your tickets after the lottery has been executed to see if you won.";
+                    text.children[2].innerHTML = "Click <b>Go Back</b> to close this window.";
+                } else if (action == "leave_lottery") {
+                    text.children[0].innerHTML += " You have left the lottery for <b>" + info['event_name'] + "</b>.";
+                    text.children[1].innerHTML = "You will no longer be considered for lottery tickets in this game.";
+                    text.children[2].innerHTML = "Click <b>Go Back</b> to close this window.";
+                } else if (action == "create_auction") {
+                    text.children[0].innerHTML += " Your ticket for <b>" + info['event_name'] + "</b> is now up for auction.";
+                    text.children[1].innerHTML = "The auction will end automatically at <b>" + formatLongDate(info['end_date']) + "</b>.";
+                    text.children[2].innerHTML = "Click <b>Go Back</b> to close this window.";
+                } else if (action == "view_auction") {
+
+                }
             }
         });
     };
@@ -162,5 +206,29 @@ function closeWindow() {
 window.onclick = function(event) {
     if (event.target.attributes.class && event.target.attributes.class.value == "modal") {
         event.target.style.display = "none";
+        location.reload();
     }
+}
+
+function populateListings() {
+    var selected = document.getElementById("gameSelect").options[gameSelect.selectedIndex].value;
+
+    var data = {
+        'action': 'auction_listings',
+        'game_id': selected,
+    };
+
+    // Request the user's balance from backend
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        url: "/requests.pyhtml",
+        error: (error) => {
+            console.log(error);
+        },
+    }).then((data) => {
+        // Show user balance on page
+        document.getElementById("auctionListings").innerHTML = data;
+    });
 }
